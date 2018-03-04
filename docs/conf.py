64d15294fa2893122021fd17ddc383911f163869
azzyghiasvand <35242194+azzyghiasvand@users.noreply.github.com>
2018-03-04 15:55:34 -0800

    changed to classic in index.py

--- docs/conf.py
+++ docs/conf.py
@@ -90,6 +90,9 @@ html_theme = 'classic'
 #
 # html_theme_options = {}
 
+html_theme_option = {
+'logo': 'logo.png' }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
